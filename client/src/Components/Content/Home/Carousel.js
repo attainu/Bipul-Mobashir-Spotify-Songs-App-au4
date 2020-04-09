51a4720a28ce5fdcfa51d36f72aea83bdbf8e55a
mobashir-raza-vst-au4 <54476228+mobashir-raza-vst-au4@users.noreply.github.com>
2020-04-09 17:27:00 +0530

    add carousel

--- client/src/Components/Content/Home/Carousel.js
+++ client/src/Components/Content/Home/Carousel.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-
-export default class Carousel extends Component {
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+export default class Carouse extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+                <Carousel centerMode centerSlidePercentage={50} showThumbs={false} showStatus={false} emulateTouch>
+                <div>
+                    <img src="https://i.ytimg.com/vi/Ps4aVpIESkc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBB9d6tpKF6j1y3SmHCIjbLaE0W1Q" />
+                </div>
+                <div>
+                    <img src="https://i.ytimg.com/vi/_iktURk0X-A/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA0VTBgvnXma3UIfCPVrt8lsk6bqA" />
+                </div>
+                <div>
+                    <img src="https://i.ytimg.com/vi/JsXw02JUrA4/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC3vyEZDCgNBxVFlzOmNnHFxKU5pA" />
+                </div>
+                <div>
+                    <img src="https://i.ytimg.com/vi/8qovC498OIY/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBbF1moZI6xj7o2Owz2Fz90jpvnnA" />
+                </div>
+            </Carousel>
         )
     }
 }
