9f52d95c9f34afa595dbb4bf9def0aacc9205748
mobashir-raza-vst-au4 <54476228+mobashir-raza-vst-au4@users.noreply.github.com>
2020-04-11 20:20:20 +0530

    musicPlayer

--- client/src/Components/MusicPlayer/MusicPlayer.js
+++ client/src/Components/MusicPlayer/MusicPlayer.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
 
 export default class MusicPlayer extends Component {
     state={
@@ -10,7 +8,7 @@ export default class MusicPlayer extends Component {
     handlePlay = () => {
             console.log("playing>>")
             var songs = this.state.song;
-            songs.src = "https://r1---sn-cvh7knek.googlevideo.com/videoplayback?expire=1586533100&ei=jD6QXv3sMIicvgST7JXQCw&ip=13.127.166.170&id=o-ADv_01EtWlNLXeCSDVcDHfOsxaiRAdiSPdiWE1gJMlKt&itag=140&source=youtube&requiressl=yes&mh=Vb&mm=31%2C26&mn=sn-cvh7knek%2Csn-qxa7sn7l&ms=au%2Conr&mv=u&mvi=0&pl=23&vprv=1&mime=audio%2Fmp4&gir=yes&clen=1456961&dur=89.977&lmt=1579353062561880&mt=1586510966&fvip=1&keepalive=yes&c=WEB&txp=5431432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl&lsig=ALrAebAwRQIgKoIJHoRvriJatQASo1KSksVaKhbsDJ5dsfmu0vnjg_ACIQCSQeWw63Kg5_KWNT_IGJiIhWQLFFca-XhAlJq4YI3TbQ%3D%3D&ratebypass=yes&sig=AJpPlLswRQIgeF245C6ToREgo2fYXWMfaZAet3qOBzD7cqOuO65e2F4CIQDqZUPedV6w9I3wGHbHMTSl7NtNvWZqpcpMeuvFBVJd8g%3D%3D";
+            songs.src = "https://r5---sn-cvh7knes.googlevideo.com/videoplayback?expire=1586605317&ei=pFiRXvKIOt6qoQPMu4WYCw&ip=13.127.166.170&id=o-ABlpozsPg9bmTmhABEAKHLWyPXo-ENYbd8B-g2iHr0kP&itag=140&source=youtube&requiressl=yes&mh=Sn&mm=31%2C26&mn=sn-cvh7knes%2Csn-qxa7snee&ms=au%2Conr&mv=u&mvi=4&pl=23&gcr=in&vprv=1&mime=audio%2Fmp4&gir=yes&clen=4557574&dur=281.565&lmt=1584770154182412&mt=1586583488&fvip=5&keepalive=yes&c=WEB&txp=5431432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl&lsig=ALrAebAwRAIgQa8g_cZcwdya_SRfVDm6eVelA07Pqd4LuAYgIy7XzwUCICvbERv7S_QWTU-sjwb4xkUCe_irrMBZc-eIs8DVnfSi&ratebypass=yes&sig=AJpPlLswRAIgR0zevUR8nJTL4ebt8mooY-owYwbcouXsWkCLTNcS-3MCIBCfs9zPmnWYXiGf32XaoIzjAjZmhz277aeuCGp6xlI_";
             
             songs.volume = 1;
             songs.play();
@@ -20,6 +18,7 @@ export default class MusicPlayer extends Component {
     handlePause = () => {
             this.state.song.pause()
     }
+
     handleMute = () => {
         if(this.state.song.muted){
             this.state.song.muted = false
