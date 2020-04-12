cbaf84ed6b96875ec00af4367741cb775a71fc95
Bipul Yadav <krbipul.90@gmail.com>
2020-04-12 13:15:37 +0530

    Search

--- client/src/API/getSearch.js
+++ client/src/API/getSearch.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 import API from './api-key';
-let getSearch = async (input)=>{
-    var exclude = " -shows -movies -funny -comedy -trailer -news -teaser -sports -cricket -serial -bhojpuri -cartoon -football";
+let getSearch = async (value)=>{
+  if(value.includes("song")){
+    console.log("true")
+  }else{
+    value = `${value} song`
+    console.log("added")
+  }
+let input =encodeURIComponent(value)
+  console.log("Search Value here..",input);
+    let hide = " -bhojpuri";
+    let exclude = encodeURIComponent(hide)
     try {
-        const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${input}${exclude}&key=${API}`);
+        const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${input}&regionCode=IN&safeSearch=strict&topicId=music&type=video&videoCategoryId=10&key=${API}`);
        console.log("response in getSearch>>",response);
        return response;
       } catch (error) {
